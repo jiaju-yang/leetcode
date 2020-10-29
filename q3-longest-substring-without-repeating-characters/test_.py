65829fb9e12b41fe63aa9e91bca56a30eebe8854
jiaju-yang <jiaju.yang@outlook.com>
2020-10-29 09:47:37 +0800

    Normalize format

--- q3-longest-substring-without-repeating-characters/test_.py
+++ q3-longest-substring-without-repeating-characters/test_.py
@@ -4,11 +4,11 @@ solve = Solution().lengthOfLongestSubstring
 
 
 def test_default():
-    assert solve("abcabcbb") == 3
-    assert solve("pwwkew") == 3
+    assert solve('abcabcbb') == 3
+    assert solve('pwwkew') == 3
 
 
 def test_corner_cases():
-    assert solve("bbbbb") == 1
-    assert solve("b") == 1
-    assert solve("") == 0
+    assert solve('bbbbb') == 1
+    assert solve('b') == 1
+    assert solve('') == 0
