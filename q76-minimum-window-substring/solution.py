8042d1f3efd8f02a5b75441c5fa49ef2fee985c3
jiaju-yang <jiaju.yang@outlook.com>
2020-10-29 10:56:05 +0800

    Improve the DP solution of question 76

--- q76-minimum-window-substring/solution.py
+++ q76-minimum-window-substring/solution.py
@@ -14,16 +14,16 @@ class DPSolution:
         need = defaultdict(int)
         for c in t:
             need[c] += 1
+        uncovered = len(t)
         seen = {c: [] for c in t}
         for i, c in enumerate(s):
             if c in need:
                 seen[c].append(i)
                 if len(seen[c]) > need[c]:
                     seen[c].pop(0)
-                current_size = 0
-                for indexes in seen.values():
-                    current_size += len(indexes)
-                if current_size == len(t):
+                else:
+                    uncovered -= 1
+                if uncovered == 0:
                     start = min(indexes[0] for indexes in seen.values())
                     current_length = i - start + 1
                     if len(shortest_s) == 0 or current_length < len(shortest_s):
