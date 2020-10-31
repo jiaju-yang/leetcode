6ef80be6249c36da4fbe758e79d9469388a55de9
jiaju-yang <jiaju.yang@outlook.com>
2020-10-31 11:19:18 +0800

    Improve the two pointers solution of question 11 with less code

--- q11-container-with-most-water/solution.py
+++ q11-container-with-most-water/solution.py
@@ -13,22 +13,14 @@ class Solution:
         if len(heights) < 2:
             return 0
         left, right = 0, len(heights) - 1
-        current_height = min(heights[left], heights[right])
-        max_area = current_height * (right - left)
+        max_area = 0
         while left < right:
-            if heights[left] < heights[right]:
-                new_left = left + 1
-                while new_left < right and heights[new_left] <= heights[left]:
-                    new_left += 1
-                left = new_left
-            else:
-                new_right = right - 1
-                while new_right > left and heights[new_right] <= heights[right]:
-                    new_right -= 1
-                right = new_right
-            if left < right:
-                max_area = max(
+            max_area = max(
                     max_area, (right - left) * min(heights[left], heights[right]))
+            if heights[left] < heights[right]:
+                left += 1
+            else:
+                right -= 1
         return max_area
 
 # @lc code=end
