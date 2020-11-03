84a5ac6837c7a463191c841e698cea1af785e5d3
jiaju-yang <jiaju.yang@outlook.com>
2020-11-03 11:12:33 +0800

    Optimize the solution of question 56 by less code

--- q56-merge-intervals/solution.py
+++ q56-merge-intervals/solution.py
@@ -11,16 +11,14 @@ from operator import itemgetter
 
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        if not intervals:
-            return []
         intervals.sort(key=itemgetter(0))
-        result = [intervals[0]]
-        for i in range(1, len(intervals)):
-            if intervals[i][0] <= result[-1][1]:
-                if result[-1][1] < intervals[i][1]:
-                    result[-1][1] = intervals[i][1]
+        result = []
+        for interval in intervals:
+            if result and interval[0] <= result[-1][1]:
+                if result[-1][1] < interval[1]:
+                    result[-1][1] = interval[1]
             else:
-                result.append(intervals[i])
+                result.append(interval)
         return result
 
 # @lc code=end
