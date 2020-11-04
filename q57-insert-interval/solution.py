9d4594da911ea4b29e4fae7847bb68ef9cde4758
jiaju-yang <jiaju.yang@outlook.com>
2020-11-04 10:41:01 +0800

    Improve the solution of question 57 with less code

--- q57-insert-interval/solution.py
+++ q57-insert-interval/solution.py
@@ -11,22 +11,17 @@ from typing import List
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
         insert_location = 0
-        while insert_location < len(intervals):
-            if intervals[insert_location][0] < newInterval[0]:
-                insert_location += 1
-            else:
-                break
-        if insert_location > 0:
-            previous = insert_location - 1
-            if newInterval[0] <= intervals[previous][1]:
-                newInterval[0] = intervals[previous][0]
-                newInterval[1] = max(newInterval[1], intervals[previous][1])
-                intervals.pop(previous)
-                insert_location -= 1
-        subsequent = insert_location
-        while subsequent < len(intervals) and intervals[subsequent][0] <= newInterval[1]:
-            newInterval[1] = max(newInterval[1], intervals[subsequent][1])
+        while insert_location < len(intervals) and intervals[insert_location][0] < newInterval[0]:
+            insert_location += 1
+        previous = insert_location - 1
+        if insert_location > 0 and newInterval[0] <= intervals[previous][1]:
+            intervals[previous][1] = max(newInterval[1], intervals[previous][1])
+            insert_location = previous
+        else:
+            intervals.insert(insert_location, newInterval)
+        subsequent = insert_location + 1
+        while subsequent < len(intervals) and intervals[subsequent][0] <= intervals[insert_location][1]:
+            intervals[insert_location][1] = max(intervals[insert_location][1], intervals[subsequent][1])
             intervals.pop(subsequent)
-        intervals.insert(insert_location, newInterval)
         return intervals
 # @lc code=end
