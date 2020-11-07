8b2f225d17b98b83b263d17752fb1e58fff0f0a5
jiaju-yang <jiaju.yang@outlook.com>
2020-11-07 09:46:25 +0800

    Improve the solution of question 268 with exact one iteration

--- q268-missing-number/solution.py
+++ q268-missing-number/solution.py
@@ -11,13 +11,12 @@ from typing import List
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         nums.append(None)
+        none_position = len(nums) - 1
         for i in range(len(nums)):
             while nums[i] is not None and i != nums[i]:
                 swap_index = nums[i]
                 nums[i], nums[swap_index] = nums[swap_index], nums[i]
-        for i in range(len(nums)):
             if nums[i] is None:
-                return i
-
-
+                none_position = i
+        return none_position
 # @lc code=end
