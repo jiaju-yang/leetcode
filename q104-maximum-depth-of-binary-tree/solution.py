35449d57fd023a7e544386a4ee208cf9417bd166
jiaju-yang <jiaju.yang@outlook.com>
2020-11-10 09:35:04 +0800

    Add the simplest DFS solution to question 104

--- q104-maximum-depth-of-binary-tree/solution.py
+++ q104-maximum-depth-of-binary-tree/solution.py
@@ -42,5 +42,12 @@ class DFSSolution:
         return depth
 
 
-Solution = DFSSolution
+class AnotherDFSSolution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+Solution = AnotherDFSSolution
 # @lc code=end
