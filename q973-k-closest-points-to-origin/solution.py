01b3872d0bbb251ac03614e9acb9725a0b79ef18
jiaju-yang <jiaju.yang@outlook.com>
2020-11-27 10:28:45 +0800

    Add an another heap solution to question 973

--- q973-k-closest-points-to-origin/solution.py
+++ q973-k-closest-points-to-origin/solution.py
@@ -6,7 +6,7 @@
 
 # @lc code=start
 from typing import List
-from heapq import heapify, heappush, heappushpop
+from heapq import heapify, heappush, heappushpop, heappop
 
 
 class HeapSolution:
@@ -21,5 +21,17 @@ class HeapSolution:
         return [[distance[1], distance[2]] for distance in heap]
 
 
-Solution = HeapSolution
+class AnotherHeapSolution:
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        distances = [((point[0]**2) + (point[1]**2), point[0], point[1])
+                     for point in points]
+        heapify(distances)
+        result = []
+        for _ in range(k):
+            distance = heappop(distances)
+            result.append([distance[1], distance[2]])
+        return result
+
+
+Solution = AnotherHeapSolution
 # @lc code=end
