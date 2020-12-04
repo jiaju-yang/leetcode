eaca578e482dd37e64cef539dd3f02bee99392f7
jiaju-yang <jiaju.yang@outlook.com>
2020-12-04 10:52:31 +0800

    Add a DP solution to question 474

--- q474-ones-and-zeroes/solution.py
+++ q474-ones-and-zeroes/solution.py
@@ -9,7 +9,7 @@ from typing import List
 from collections import Counter
 
 
-class Solution:
+class BruteForceSolution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
         def find_max_in_subset(start, target_m, target_n, curr_size):
             nonlocal max_size
@@ -21,7 +21,7 @@ class Solution:
                 return
             curr_m, curr_n = self.count_01s(strs[start])
             find_max_in_subset(start + 1, target_m - curr_m,
-                   target_n - curr_n, curr_size + 1)
+                               target_n - curr_n, curr_size + 1)
             find_max_in_subset(start + 1, target_m, target_n, curr_size)
         max_size = 0
         find_max_in_subset(0, m, n, 0)
@@ -31,4 +31,29 @@ class Solution:
         counter = Counter(seq)
         return (counter['0'] if '0' in counter else 0,
                 counter['1'] if '1' in counter else 0)
+
+
+class DPSolution:
+    def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
+        dp = [[[None] * (n+1) for i in range(m+1)]
+              for i in range(len(strs) + 1)]
+        dp[0][0][0] = 0
+        for start in range(1, len(strs)+1):
+            curr_m, curr_n = self.count_01s(strs[start-1])
+            for i in range(m+1):
+                for j in range(n+1):
+                    if i-curr_m >= 0 and j-curr_n >= 0 and dp[start-1][i-curr_m][j-curr_n] is not None:
+                        dp[start][i][j] = dp[start-1][i-curr_m][j-curr_n] + 1
+                    if dp[start-1][i][j] is not None:
+                        dp[start][i][j] = max(
+                            dp[start-1][i][j], dp[start][i][j] or 0)
+        return max(dp[-1][i][j] or 0 for i in range(m+1) for j in range(n+1))
+
+    def count_01s(self, seq):
+        counter = Counter(seq)
+        return (counter['0'] if '0' in counter else 0,
+                counter['1'] if '1' in counter else 0)
+
+
+Solution = DPSolution
 # @lc code=end
--- q474-ones-and-zeroes/test_.py
+++ q474-ones-and-zeroes/test_.py
@@ -6,12 +6,13 @@ solve = Solution().findMaxForm
 def test_default():
     assert solve(['10', '0001', '111001', '1', '0'], 5, 3) == 4
     assert solve(['10', '0', '1'], 1, 1) == 2
+    assert solve(['10', '0001', '111001', '1', '0'], 3, 4) == 3
 
 
 def test_corner_cases():
     assert solve(['10'], 1, 1) == 1
+    assert solve(['10'], 2, 1) == 1
 
 
 def test_failure_cases():
-    assert solve(['10'], 2, 1) == 0
     assert solve([], 1, 0) == 0
