dc3b9e6c21cc56e863a49b1010271cece1064938
jiaju-yang <jiaju.yang@outlook.com>
2020-12-06 11:12:09 +0800

    Add a BFS solution to question 207

--- q207-course-schedule/solution.py
+++ q207-course-schedule/solution.py
@@ -9,15 +9,16 @@ from typing import List
 
 
 class Node:
-    def __init__(self, name, color='white', adjs=None):
+    def __init__(self, name, color='white', adjs=None, indegree=0):
         if not adjs:
             adjs = []
         self.name = name
         self.color = color
         self.adjs = adjs
+        self.indegree = indegree
 
 
-class Solution:
+class DFSSolution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
         def dfs_visit(course):
             course.color = 'gray'
@@ -46,4 +47,35 @@ class Solution:
                 if not result:
                     return False
         return True
+
+
+class BFSSolution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        courses = {}
+        for edge in prerequisites:
+            try:
+                courses[edge[0]].adjs.append(edge[1])
+            except KeyError:
+                courses[edge[0]] = Node(edge[0], adjs=[edge[1]])
+            if edge[1] not in courses:
+                courses[edge[1]] = Node(edge[1])
+            courses[edge[1]].indegree += 1
+        num_course_with_dependencies = len(courses)
+        q = deque()
+        for course in courses.values():
+            if not course.indegree:
+                q.appendleft(course)
+        visited = 0
+        while q:
+            course = q.pop()
+            for adj_course_name in course.adjs:
+                adj_course = courses[adj_course_name]
+                adj_course.indegree -= 1
+                if not adj_course.indegree:
+                    q.appendleft(adj_course)
+            visited += 1
+        return visited == num_course_with_dependencies
+
+
+Solution = BFSSolution
 # @lc code=end
--- q207-course-schedule/test_.py
+++ q207-course-schedule/test_.py
@@ -5,11 +5,16 @@ solve = Solution().canFinish
 
 def test_default():
     assert solve(2, [[1, 0]]) is True
+    assert solve(3, [[1, 0], [2, 0]]) is True
+    assert solve(8, [[1, 0], [2, 6], [1, 7], [6, 4], [7, 0], [0, 5]]) is True
 
 
 def test_corner_cases():
     assert solve(0, []) is True
+    assert solve(1, []) is True
 
 
 def test_failure_cases():
     assert solve(2, [[1, 0], [0, 1]]) is False
+    assert solve(3, [[1, 0], [1, 2], [0, 1]]) is False
+    assert solve(3, [[0, 2], [1, 2], [2, 0]]) is False
