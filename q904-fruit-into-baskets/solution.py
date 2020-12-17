a709ab44342f8a337be08d6177f3e3c41e4f42c2
jiaju-yang <jiaju.yang@outlook.com>
2020-12-17 10:22:16 +0800

    Improve the O(n) solution to question 904 with less code

--- q904-fruit-into-baskets/solution.py
+++ q904-fruit-into-baskets/solution.py
@@ -12,27 +12,16 @@ from collections import defaultdict
 class Solution:
     def totalFruit(self, tree: List[int]) -> int:
         baskets = defaultdict(int)
-        remain_basket = 2
         start = 0
         max_amount = 0
         for end, fruit_type in enumerate(tree):
-            if remain_basket > 0:
-                baskets[fruit_type] += 1
-                remain_basket = 2 - len(baskets)
-                max_amount = max(sum(baskets.values()), max_amount)
-            else:
-                if fruit_type in baskets:
-                    baskets[fruit_type] += 1
-                    max_amount = max(sum(baskets.values()), max_amount)
-                else:
-                    while start < end:
-                        baskets[tree[start]] -= 1
-                        start += 1
-                        if baskets[tree[start-1]] == 0:
-                            del baskets[tree[start-1]]
-                            break
-                    baskets[fruit_type] += 1
-                    max_amount = max(sum(baskets.values()), max_amount)
+            baskets[fruit_type] += 1
+            while len(baskets) > 2:
+                baskets[tree[start]] -= 1
+                if baskets[tree[start]] == 0:
+                    del baskets[tree[start]]
+                start += 1
+            max_amount = max(max_amount, end - start + 1)
         return max_amount
 
 # @lc code=end
