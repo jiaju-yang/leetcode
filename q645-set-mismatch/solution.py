9154352676749be34f1de8ae2650d306bb14ffc9
jiaju-yang <jiaju.yang@outlook.com>
2021-01-03 10:32:25 +0800

    Add an xor solution to question 645

--- q645-set-mismatch/solution.py
+++ q645-set-mismatch/solution.py
@@ -8,7 +8,7 @@
 from typing import List
 
 
-class Solution:
+class HashSolution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         result = [0, 0]
         for num in nums:
@@ -21,4 +21,30 @@ class Solution:
                 result[1] = i+1
         return result
 
+
+class XorSolution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        missing_xor_duplicate = 0
+        for i, num in enumerate(nums, 1):
+            missing_xor_duplicate ^= i
+            missing_xor_duplicate ^= num
+        right_most_bit_num = missing_xor_duplicate & -missing_xor_duplicate
+        xor1, xor2 = 0, 0
+        for i, num in enumerate(nums, 1):
+            if num & right_most_bit_num == 0:
+                xor1 ^= num
+            else:
+                xor2 ^= num
+            if i & right_most_bit_num == 0:
+                xor1 ^= i
+            else:
+                xor2 ^= i
+        for num in nums:
+            if num == xor1:
+                return [xor1, xor2]
+            if num == xor2:
+                return [xor2, xor1]
+
+
+Solution = XorSolution
 # @lc code=end
