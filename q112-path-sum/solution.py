ec53dae8d4205391dbb850f727fa45c7e0c11c2d
jiaju-yang <jiaju.yang@outlook.com>
2021-01-09 09:28:31 +0800

    Add a BFS solution to question 112

--- q112-path-sum/solution.py
+++ q112-path-sum/solution.py
@@ -5,7 +5,10 @@
 #
 
 # @lc code=start
+from collections import deque
 # Definition for a binary tree node.
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -13,7 +16,7 @@ class TreeNode:
         self.right = right
 
 
-class Solution:
+class DFSSolution:
     def hasPathSum(self, root: TreeNode, sum: int) -> bool:
         def dfs_visit(node, remain):
             if not node:
@@ -24,4 +27,22 @@ class Solution:
             return dfs_visit(node.left, curr_remain) or dfs_visit(node.right, curr_remain)
         return dfs_visit(root, sum)
 
+
+class BFSSolution:
+    def hasPathSum(self, root: TreeNode, sum: int) -> bool:
+        if not root:
+            return False
+        q = deque([(root, sum)])
+        while q:
+            node, remain = q.pop()
+            if not node.left and not node.right and node.val == remain:
+                return True
+            if node.left:
+                q.appendleft((node.left, remain-node.val))
+            if node.right:
+                q.appendleft((node.right, remain-node.val))
+        return False
+
+
+Solution = BFSSolution
 # @lc code=end
