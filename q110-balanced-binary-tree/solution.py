09c530b3eae9851107a835c9b95e46fcc40b2f3a
jiaju-yang <jiaju.yang@outlook.com>
2021-01-10 09:57:11 +0800

    Improve the DFS solution to question 110 with better performance

--- q110-balanced-binary-tree/solution.py
+++ q110-balanced-binary-tree/solution.py
@@ -13,7 +13,7 @@ class TreeNode:
         self.right = right
 
 
-class Solution:
+class DFSSolution:
     def isBalanced(self, root: TreeNode) -> bool:
         def dfs_visit(node):
             if not node:
@@ -26,4 +26,23 @@ class Solution:
             return balanced, max(left_height, right_height) + 1
         return dfs_visit(root)[0]
 
+
+class AnotherDFSSolution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        def dfs_visit(node):
+            if not node:
+                return 0
+            left_height = dfs_visit(node.left)
+            if left_height < 0:
+                return -1
+            right_height = dfs_visit(node.right)
+            if right_height < 0:
+                return -1
+            if abs(left_height - right_height) > 1:
+                return -1
+            return max(left_height, right_height) + 1
+        return False if dfs_visit(root) < 0 else True
+
+
+Solution = AnotherDFSSolution
 # @lc code=end
