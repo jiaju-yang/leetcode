33bbcf906e13a932f32e7ee6d2e79fc1ab819519
jiaju-yang <jiaju.yang@outlook.com>
2021-10-22 18:12:49 +0100

    Add pytest settings

--- src/q76-minimum-window-substring.py
+++ src/q76-minimum-window-substring.py
@@ -36,3 +36,21 @@ class Solution:
 
 
 # @lc code=end
+solve = Solution().minWindow
+
+
+def test_default():
+    assert solve('ADOBECODEBANC', 'ABC') == 'BANC'
+
+
+def test_failure_cases():
+    assert solve('a', '') == ''
+    assert solve('', 'a') == ''
+    assert solve('a', 'aa') == ''
+    assert solve('', '') == ''
+
+
+def test_corner_cases():
+    assert solve('a', 'a') == 'a'
+    assert solve('aa', 'a') == 'a'
+    assert solve('aa', 'aa') == 'aa'
