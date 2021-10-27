5d7d0e72910e3835477c1d798789d4384a9efc28
jiaju-yang <jiaju.yang@outlook.com>
2021-10-27 16:22:17 +0100

    Add another bottom-up DP solution to question 416

--- src/q416-partition-equal-subset-sum.py
+++ src/q416-partition-equal-subset-sum.py
@@ -4,10 +4,9 @@
 # [416] Partition Equal Subset Sum
 #
 from typing import List
-# @lc code=start
 
 
-class Solution:
+class DPSolution:
     def canPartition(self, nums: List[int]) -> bool:
         n = len(nums)
         differences = {0}
@@ -21,10 +20,26 @@ class Solution:
             return True
         return False
 
+# @lc code=start
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        if not nums:
+            return True
+        total = sum(nums)
+        target = total >> 1
+        if (target << 1) != total:
+            return False
+        dp = {0}
+        for num in nums:
+            dp.update({get+num for get in dp})
+            if target in dp:
+                return True
+        return False
+
 
 # @lc code=end
-
-
 solve = Solution().canPartition
 
 
