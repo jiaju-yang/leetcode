5a6a3b1d64571b7663ecf149552850604491b230
jiaju-yang <jiaju.yang@outlook.com>
2021-10-27 12:08:05 +0100

    Add a bottom-up DP solution to question 416

---
+++ src/q416-partition-equal-subset-sum.py
@@ -0,0 +1,41 @@
+#
+# @lc app=leetcode id=416 lang=python3
+#
+# [416] Partition Equal Subset Sum
+#
+from typing import List
+# @lc code=start
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        n = len(nums)
+        differences = {0}
+        for num in nums:
+            new_differences = set()
+            for d in differences:
+                new_differences.add(d-num)
+                new_differences.add(d+num)
+            differences = new_differences
+        if 0 in differences:
+            return True
+        return False
+
+
+# @lc code=end
+
+
+solve = Solution().canPartition
+
+
+def test_default():
+    assert solve([1, 5, 11, 5])
+    assert not solve([1, 2, 3, 5])
+
+
+def test_corner_cases():
+    assert solve([])
+    assert solve([0])
+    assert not solve([1])
+    assert solve([1, 1])
+    assert not solve([1, 2])
