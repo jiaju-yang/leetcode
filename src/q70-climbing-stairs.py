30723b9903403b23ab4b88e7d0d2c0bc16a7e1df
jiaju-yang <jiaju.yang@outlook.com>
2021-10-29 12:19:36 +0100

    Add a matrix multiplication solution to question 70

--- src/q70-climbing-stairs.py
+++ src/q70-climbing-stairs.py
@@ -11,10 +11,41 @@ class Solution:
         for i in range(n-1):
             a, b = b, a+b
         return b
+
+
+class MatrixSolution:
+    def climbStairs(self, n: int) -> int:
+        result = self.matrix_multiply(
+            self.logorithm_matrix_multiply(n), [[1], [1]])
+        return result[1][0]
+
+    def logorithm_matrix_multiply(self, n):
+        start = [[1, 1],
+                 [1, 0]]
+        if n == 1:
+            return start
+        if n % 2 == 0:
+            half = self.logorithm_matrix_multiply(n / 2)
+            return self.matrix_multiply(half, half)
+        else:
+            return self.matrix_multiply(self.logorithm_matrix_multiply(n-1), start)
+
+    def matrix_multiply(self, m1, m2):
+        result = [[0]*len(m2[0]) for _ in range(len(m1))]
+        for i in range(len(result)):
+            for j in range(len(result[0])):
+                for k in range(len(m1[0])):
+                    result[i][j] += (m1[i][k] * m2[k][j])
+        return result
 # @lc code=end
 
 
-solve = Solution().climbStairs
+solve = MatrixSolution().climbStairs
+
+
+def test_matrix_multiply():
+    assert MatrixSolution().matrix_multiply(
+        [[1, 2], [3, 4]], [[5, 6], [7, 8]]) == [[19, 22], [43, 50]]
 
 
 def test_default():
