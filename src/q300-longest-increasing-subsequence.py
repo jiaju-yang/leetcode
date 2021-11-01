2f476ea7e721faab50dbae4f079742caa8e310d9
jiaju-yang <jiaju.yang@outlook.com>
2021-11-01 11:17:31 +0000

    Add a DP solution and a binary search solution to question 300

---
+++ src/q300-longest-increasing-subsequence.py
@@ -0,0 +1,61 @@
+#
+# @lc app=leetcode id=300 lang=python3
+#
+# [300] Longest Increasing Subsequence
+#
+from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        dp = [1] * (len(nums) + 1)
+        dp[0] = 0
+        for i in range(1, len(nums)):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    dp[i+1] = max(dp[i+1], dp[j+1] + 1)
+        return max(dp)
+
+# @lc code=start
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        l = [nums[0]]
+        for i in range(1, len(nums)):
+            if nums[i] > l[-1]:
+                l.append(nums[i])
+            else:
+                index = self.binary_search(l, nums[i])
+                if l[index] > nums[i]:
+                    l[index] = nums[i]
+        return len(l)
+
+    def binary_search(self, l, target):
+        i, j = 0, len(l) - 1
+        while i <= j:
+            middle = (i + j) >> 1
+            if l[middle] == target:
+                return middle
+            elif l[middle] < target:
+                i = middle + 1
+            else:
+                j = middle - 1
+        return i
+# @lc code=end
+
+
+solve = Solution().lengthOfLIS
+
+
+def test_default():
+    assert solve([10, 9, 2, 5, 3, 7, 101, 18]) == 4
+    assert solve([0, 1, 0, 3, 2, 3]) == 4
+    assert solve([7, 7, 7, 7, 7, 7, 7]) == 1
+
+
+def test_corner_cases():
+    assert solve([1]) == 1
+    assert solve([1, 2]) == 2
+    assert solve([-1, 2]) == 2
+    assert solve([-1]) == 1
