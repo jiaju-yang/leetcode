99984d84201aaedd2dd2d26371cec0bf418c35ce
jiaju-yang <jiaju.yang@outlook.com>
2021-11-02 14:46:17 +0000

    Improve the DP solution to question 198

--- src/q198-house-robber.py
+++ src/q198-house-robber.py
@@ -9,11 +9,11 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        dp = [0] * (len(nums)+1)
-        dp[0], dp[1] = 0, nums[0]
+        second_last, last = 0, nums[0]
         for i in range(2, len(nums)+1):
-            dp[i] = max(dp[i-2] + nums[i-1], dp[i-1])
-        return dp[-1]
+            current = max(second_last + nums[i-1], last)
+            second_last, last = last, current
+        return last
 
 
 # @lc code=end
