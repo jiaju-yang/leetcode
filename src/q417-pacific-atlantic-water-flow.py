b4354d1b9087ee8710fb0fb2c2e1c4aa1da3d1d2
jiaju-yang <jiaju.yang@outlook.com>
2021-11-07 15:16:56 +0000

    Improve the DFS solution to question 417

--- src/q417-pacific-atlantic-water-flow.py
+++ src/q417-pacific-atlantic-water-flow.py
@@ -4,43 +4,28 @@
 # [417] Pacific Atlantic Water Flow
 #
 from typing import List
-from collections import defaultdict
 
 # @lc code=start
 
 
 class Solution:
+    directions = [[0, 1], [1, 0], [0, -1], [-1, 0]]
+
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        adjs = defaultdict(list)
         m = len(heights)
         n = len(heights[0])
-        neighbors = [[0, 1], [1, 0], [0, -1], [-1, 0]]
-        for i in range(m):
-            for j in range(n):
-                for neighbor_i_off, neighbor_j_off in neighbors:
-                    neighbor_i, neighbor_j = i + neighbor_i_off, j + neighbor_j_off
-                    if 0 <= neighbor_i < m and 0 <= neighbor_j < n and heights[i][j] <= heights[neighbor_i][neighbor_j]:
-                        adjs[(i, j)].append((neighbor_i, neighbor_j))
 
         pacific_ocean_access = [[False] * n for _ in range(m)]
         for i in range(n):
-            pacific_ocean_access[0][i] = True
+            self.dfs((0, i), pacific_ocean_access, heights, m, n)
         for i in range(m):
-            pacific_ocean_access[i][0] = True
-        for i in range(n):
-            self.dfs((0, i), pacific_ocean_access, adjs)
-        for i in range(m):
-            self.dfs((i, 0), pacific_ocean_access, adjs)
+            self.dfs((i, 0), pacific_ocean_access, heights, m, n)
 
         atlantic_ocean_access = [[False] * n for _ in range(m)]
         for i in range(n):
-            atlantic_ocean_access[-1][i] = True
+            self.dfs((m-1, i), atlantic_ocean_access, heights, m, n)
         for i in range(m):
-            atlantic_ocean_access[i][-1] = True
-        for i in range(n):
-            self.dfs((m-1, i), atlantic_ocean_access, adjs)
-        for i in range(m):
-            self.dfs((i, n-1), atlantic_ocean_access, adjs)
+            self.dfs((i, n-1), atlantic_ocean_access, heights, m, n)
 
         result = []
         for i in range(m):
@@ -49,15 +34,21 @@ class Solution:
                     result.append([i, j])
         return result
 
-    def dfs(self, node, states, adjs):
+    def dfs(self, node, states, heights, m, n):
+        if states[node[0]][node[1]]:
+            return
         states[node[0]][node[1]] = True
-        for adj in adjs[node]:
-            if not states[adj[0]][adj[1]]:
-                self.dfs(adj, states, adjs)
+        for i, j in self.neighbors(node[0], node[1], heights, m, n):
+            self.dfs((i, j), states, heights, m, n)
+
+    def neighbors(self, i, j, heights, m, n):
+        for neighbor_i_off, neighbor_j_off in self.directions:
+            neighbor_i, neighbor_j = i + neighbor_i_off, j + neighbor_j_off
+            if 0 <= neighbor_i < m and 0 <= neighbor_j < n and heights[i][j] <= heights[neighbor_i][neighbor_j]:
+                yield (neighbor_i, neighbor_j)
 
 
 # @lc code=end
-
 
 solve = Solution().pacificAtlantic
 
