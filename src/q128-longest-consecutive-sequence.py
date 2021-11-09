0fe52c78dab3669c1b2116b756fa272791824c92
jiaju-yang <jiaju.yang@outlook.com>
2021-11-09 11:43:14 +0000

    Add a DFS solution to question 128

---
+++ src/q128-longest-consecutive-sequence.py
@@ -0,0 +1,48 @@
+#
+# @lc app=leetcode id=128 lang=python3
+#
+# [128] Longest Consecutive Sequence
+#
+from typing import List
+
+# @lc code=start
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        states = {num: False for num in nums}
+        max_length = 0
+        for num in nums:
+            if not states[num]:
+                max_length = max(max_length, self.dfs(num, states))
+        return max_length
+
+    def dfs(self, num, states):
+        states[num] = True
+        length = 1
+        for neighbor in self.neighbors(num):
+            if not states.get(neighbor, True):
+                length += self.dfs(neighbor, states)
+        return length
+
+    def neighbors(self, num):
+        yield num + 1
+        yield num - 1
+# @lc code=end
+
+
+solve = Solution().longestConsecutive
+
+
+def test_default():
+    assert solve([100, 4, 200, 1, 3, 2]) == 4
+    assert solve([0, 3, 7, 2, 5, 8, 4, 6, 0, 1]) == 9
+
+
+def test_corner_cases():
+    assert solve([1]) == 1
+    assert solve([]) == 0
+    assert solve([1, 2]) == 2
+    assert solve([1, 3]) == 1
+    assert solve([-1]) == 1
+    assert solve([-1, -2]) == 2
