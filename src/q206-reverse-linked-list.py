71dd8a78be37a51d393425e32bcc28c9da206ec6
jiaju-yang <jiaju.yang@outlook.com>
2021-11-17 15:28:19 +0000

    Improve the solution to question 206

--- src/q206-reverse-linked-list.py
+++ src/q206-reverse-linked-list.py
@@ -13,10 +13,7 @@ class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         previous, current = None, head
         while current:
-            next = current.next
-            current.next = previous
-            previous = current
-            current = next
+            current.next, previous, current = previous, current, current.next
         return previous
 
 
