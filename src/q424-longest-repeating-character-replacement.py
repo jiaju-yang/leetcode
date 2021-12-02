d29037c7e11ade593aacddd672f596d7455ae53f
jiaju-yang <jiaju.yang@outlook.com>
2021-12-02 16:17:51 +0000

    Improve the sliding window solution to question 424

--- src/q424-longest-repeating-character-replacement.py
+++ src/q424-longest-repeating-character-replacement.py
@@ -11,16 +11,14 @@ from collections import defaultdict
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         counter = defaultdict(int)
-        left = -1
-        max_length = 0
+        left, max_count, right = -1, 0, 0
         for right, c in enumerate(s):
             counter[c] += 1
-            max_count = max(counter.values())
+            max_count = max(max_count, counter[c])
             if right - left - max_count > k:
                 left += 1
                 counter[s[left]] -= 1
-            max_length = max(max_length, right - left)
-        return max_length
+        return right - left
 
 
 # @lc code=end
@@ -33,8 +31,6 @@ def test_default():
 
 
 def test_corner_cases():
-    assert solve('', 0) == 0
-    assert solve('', 1) == 0
     assert solve('a', 0) == 1
     assert solve('aa', 0) == 2
     assert solve('aa', 1) == 2
