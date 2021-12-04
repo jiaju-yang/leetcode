daf8d58198e9551c10f2d223d76952147aea08cc
jiaju-yang <jiaju.yang@outlook.com>
2021-12-04 16:19:35 +0000

    Improve the solution to question 49 using a hash map

--- src/q49-group-anagrams.py
+++ src/q49-group-anagrams.py
@@ -4,7 +4,7 @@
 # [49] Group Anagrams
 #
 from typing import List
-from collections import Counter
+from collections import Counter, defaultdict
 
 # @lc code=start
 
@@ -13,17 +13,18 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         it = iter(strs)
         result = [[next(it)]]
-        counters = [Counter(result[0][0])]
+        counters = defaultdict(list)
+        counters[len(result[0][0])].append((0, Counter(result[0][0])))
         for s in it:
             counter = Counter(s)
             matched = False
-            for i, c in enumerate(counters):
+            for i, c in counters[len(s)]:
                 if counter == c:
                     result[i].append(s)
                     matched = True
                     break
             if not matched:
-                counters.append(counter)
+                counters[len(s)].append((len(result), counter))
                 result.append([s])
         return result
 
@@ -36,8 +37,11 @@ solve = Solution().groupAnagrams
 def test_default():
     assert solve(['eat', 'tea', 'tan', 'ate', 'nat', 'bat']) == [
         ['eat', 'tea', 'ate'], ['tan', 'nat'], ['bat']]
+    assert solve(['stop', 'pots', 'reed', '', 'tops', 'deer', 'opts', '']) == [
+        ['stop', 'pots', 'tops', 'opts'], ['reed', 'deer'], ['', '']]
 
 
 def test_corner_cases():
     assert solve(['']) == [['']]
     assert solve(['a']) == [['a']]
+    assert solve(['', 'b']) == [[''], ['b']]
