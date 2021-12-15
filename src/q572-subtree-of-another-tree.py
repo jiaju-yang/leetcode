10796d71948f11d2cf25a95513f25b09fe0aadda
jiaju-yang <jiaju.yang@outlook.com>
2021-12-15 15:23:12 +0000

    Add a merkle tree solution to question 572

--- src/q572-subtree-of-another-tree.py
+++ src/q572-subtree-of-another-tree.py
@@ -25,15 +25,41 @@ class TreeSolution:
         if nodea and nodeb:
             return nodea.val == nodeb.val and self.is_same(nodea.left, nodeb.left) and self.is_same(nodea.right, nodeb.right)
         return nodea is nodeb
-# @lc code=start
 
 
-class Solution:
+class SerializationSolution:
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
         return self.serialize(subRoot) in self.serialize(root)
 
     def serialize(self, node):
         return f'({node.val},{self.serialize(node.left)},{self.serialize(node.right)})' if node else ''
+
+# @lc code=start
+
+
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if not subRoot:
+            return True
+        self.merkle(root)
+        self.merkle(subRoot)
+        return self.traversal(root, subRoot.hash)
+
+    def traversal(self, node, hash_val):
+        if not node:
+            return False
+        return node.hash == hash_val or self.traversal(node.left, hash_val) or self.traversal(node.right, hash_val)
+
+    def merkle(self, node, level=0):
+        if not node:
+            return
+        self.merkle(node.left, level=level+1)
+        self.merkle(node.right, level=level+1)
+        hash_val = hash(str(node.val))
+        node.hash = hash(
+            (hash_val,
+             node.left.hash if node.left else None,
+             node.right.hash if node.right else None,))
 
 
 # @lc code=end
@@ -52,3 +78,7 @@ def test_corner_cases():
     assert solve(construct_tree([2, 1]), construct_tree([1]))
     assert not solve(construct_tree([1, 2]), construct_tree([1]))
     assert not solve(construct_tree([1, 2, 3]), construct_tree([1, 2]))
+    assert not solve(construct_tree(
+        [4, 1, None, None, 2]), construct_tree([1, 4, None, None, 2]))
+    assert not solve(construct_tree(
+        [1, 2, 3]), construct_tree([1, 3, 2]))
