3af140a334c7d5457b64ae8813ddba284c66e57f
jiaju-yang <jiaju.yang@outlook.com>
2021-12-24 15:31:14 +0000

    Improve the trie tree solution to question 212

--- src/q212-word-search-ii.py
+++ src/q212-word-search-ii.py
@@ -12,7 +12,7 @@ from collections import defaultdict
 class TrieNode:
     def __init__(self, exist=False) -> None:
         self.leaves = defaultdict(TrieNode)
-        self.exist = exist
+        self.is_word = exist
 
     def __getitem__(self, key):
         return self.leaves[key]
@@ -20,45 +20,49 @@ class TrieNode:
     def __setitem__(self, key, value):
         self.leaves[key] = value
 
-    def search(self, word):
+    def insert(self, word):
         cur = self
         for c in word:
             cur = cur[c]
-            if not cur.exist:
-                return False
-        return True
+        cur.is_word = True
+
+    def __bool__(self):
+        return bool(self.leaves) or self.is_word
 
 
 class Solution:
     adjs = ((-1, 0), (1, 0), (0, -1), (0, 1))
 
     def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
-        trie_tree = self.construct_trie_tree(board, len(max(words, key=len)))
+        trie_tree = self.construct_trie_tree(words)
         result = []
-        for word in words:
-            if trie_tree.search(word):
-                result.append(word)
-        return result
-
-    def construct_trie_tree(self, matrix, length):
-        tree = TrieNode()
-        m, n = len(matrix), len(matrix[0])
+        m, n = len(board), len(board[0])
         for i in range(m):
             for j in range(n):
-                self.dfs(tree, matrix, i, j, length-1)
+                self.dfs(trie_tree, board, i, j, result, '')
+        return result
+
+    def construct_trie_tree(self, words):
+        tree = TrieNode()
+        for word in words:
+            tree.insert(word)
         return tree
 
-    def dfs(self, tree, matrix, start_i, start_j, remain):
+    def dfs(self, tree, matrix, start_i, start_j, result, path):
         c, matrix[start_i][start_j] = matrix[start_i][start_j], False
-        tree[c].exist = True
-        if not remain:
+        next_path = path+c
+        tree = tree[c]
+        if tree.is_word:
+            result.append(next_path)
+            tree.is_word = False
+        if not tree:
             matrix[start_i][start_j] = c
             return
         m, n = len(matrix), len(matrix[0])
         for x, y in self.adjs:
             next_i, next_j = start_i + x, start_j + y
             if 0 <= next_i < m and 0 <= next_j < n and matrix[next_i][next_j]:
-                self.dfs(tree[c], matrix, next_i, next_j, remain - 1)
+                self.dfs(tree, matrix, next_i, next_j, result, next_path)
         matrix[start_i][start_j] = c
         return
 
@@ -71,8 +75,11 @@ def test_default():
     assert solve([['o', 'a', 'a', 'n'], ['e', 't', 'a', 'e'], ['i', 'h', 'k', 'r'], [
                  'i', 'f', 'l', 'v']], ['oath', 'pea', 'eat', 'rain']) == ['oath', 'eat']
     assert solve([['a', 'b'], ['c', 'd']], ['abcb']) == []
-    assert solve([['a', 'b', 'c'], ['a', 'e', 'd'], ['a', 'f', 'g']], ['abcdefg', 'gfedcbaaa',
-                 'eaabcdgfa', 'befa', 'dgc', 'ade']) == ['abcdefg', 'gfedcbaaa', 'eaabcdgfa', 'befa', ]
+
+    result = solve([['a', 'b', 'c'], ['a', 'e', 'd'], ['a', 'f', 'g']], ['abcdefg', 'gfedcbaaa',
+                                                                         'eaabcdgfa', 'befa', 'dgc', 'ade'])
+    assert len(result) == 4
+    assert set(result) == {'abcdefg', 'gfedcbaaa', 'eaabcdgfa', 'befa'}
 
 
 def test_corner_cases():
