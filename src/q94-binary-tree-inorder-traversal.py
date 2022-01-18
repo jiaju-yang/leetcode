32e8d5ff182574751b2079fde55126f333ebdc8e
jiaju-yang <jiaju.yang@outlook.com>
2022-01-18 15:05:57 +0000

    Add a recursive solution to question 94

---
+++ src/q94-binary-tree-inorder-traversal.py
@@ -0,0 +1,29 @@
+#
+# @lc app=leetcode id=94 lang=python3
+#
+# [94] Binary Tree Inorder Traversal
+#
+from .tree_tools import *
+from typing import Optional, List
+
+# @lc code=start
+
+
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        return self.inorderTraversal(root.left) + [root.val] + self.inorderTraversal(root.right)
+
+
+# @lc code=end
+solve = Solution().inorderTraversal
+
+
+def test_default():
+    assert solve(construct_tree([1, 2, None, 3, 4])) == [3, 2, 4, 1]
+
+
+def test_corner_cases():
+    assert solve(construct_tree([1])) == [1]
+    assert solve(None) == []
