af798775114ee7bc47312d9b76d65c0a9d14a4cc
jiaju-yang <jiaju.yang@outlook.com>
2022-02-15 12:05:41 +0000

    Add a O(n) solution to question 560

--- src/q560-subarray-sum-equals-k.py
+++ src/q560-subarray-sum-equals-k.py
@@ -3,6 +3,7 @@
 #
 # [560] Subarray Sum Equals K
 #
+from collections import defaultdict
 from typing import List
 
 # @lc code=start
@@ -10,13 +11,13 @@ from typing import List
 
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        count = 0
-        for i in range(len(nums)):
-            current_sum = 0
-            for j in range(i, len(nums)):
-                current_sum += nums[j]
-                if current_sum == k:
-                    count += 1
+        previous_sums = defaultdict(int)
+        previous_sums[0] = 1
+        cur_sum, count = 0, 0
+        for num in nums:
+            cur_sum += num
+            count += previous_sums[cur_sum - k]
+            previous_sums[cur_sum] += 1
         return count
 
 
@@ -34,3 +35,5 @@ def test_corner_cases():
     assert solve([1], 0) == 0
     assert solve([-1], 1) == 0
     assert solve([-1, -1], -1) == 2
+    assert solve([0], 0) == 1
+    assert solve([0, 0], 0) == 3
