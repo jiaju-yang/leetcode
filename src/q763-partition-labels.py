dfe0618da60f0a4208059d3728efe0b867b469a2
jiaju-yang <jiaju.yang@outlook.com>
2022-02-17 14:13:52 +0000

    Optimize again the O(n) solution to question 763

--- src/q763-partition-labels.py
+++ src/q763-partition-labels.py
@@ -11,25 +11,20 @@ from collections import Counter
 
 class Solution:
     def partitionLabels(self, s: str) -> List[int]:
-        total = Counter(s)
-        so_far = set()
+        total, so_far = Counter(s), set()
+        remain = last_i = 0
         ret = []
-        last_i = 0
         for i, c in enumerate(s):
+            if c not in so_far:
+                so_far.add(c)
+                remain += total[c]
             total[c] -= 1
-            so_far.add(c)
-            if total[c] == 0:
-                if self.are_all_zero(so_far, total):
-                    ret.append(i+1 - last_i)
-                    last_i = i+1
-                    so_far = set()
+            remain -= 1
+            if remain == 0:
+                ret.append(i+1 - last_i)
+                last_i = i+1
+                so_far = set()
         return ret
-
-    def are_all_zero(self, chars, counter):
-        for c in chars:
-            if counter[c] != 0:
-                return False
-        return True
 
 
 # @lc code=end
