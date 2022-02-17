1dc1ed530f0d143a772a359fef1eb29f0a8a8e35
jiaju-yang <jiaju.yang@outlook.com>
2022-02-17 13:58:08 +0000

    Add a O(n) solution to question 763

---
+++ src/q763-partition-labels.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode id=763 lang=python3
+#
+# [763] Partition Labels
+#
+from typing import List
+from collections import Counter
+
+# @lc code=start
+
+
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+        total = Counter(s)
+        so_far = Counter()
+        ret = []
+        last_i = 0
+        for i, c in enumerate(s):
+            total[c] -= 1
+            so_far[c] += 1
+            if total[c] == 0:
+                if self.are_all_zero(so_far.keys(), total):
+                    ret.append(i+1 - last_i)
+                    last_i = i+1
+                    so_far = Counter()
+        return ret
+
+    def are_all_zero(self, chars, counter):
+        for c in chars:
+            if counter[c] != 0:
+                return False
+        return True
+
+
+# @lc code=end
+solve = Solution().partitionLabels
+
+
+def test_default():
+    assert solve('ababcbacadefegdehijhklij') == [9, 7, 8]
+    assert solve('eccbbbbdec') == [10]
+
+
+def test_corner_cases():
+    assert solve('a') == [1]
+    assert solve('aa') == [2]
+    assert solve('ab') == [1, 1]
