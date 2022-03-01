52d972ed0dd99a83a9b1fad9e166954d2cfd57f2
jiaju-yang <jiaju.yang@outlook.com>
2022-03-01 16:52:47 +0000

    Optimize the stack solution to question 239

--- src/q239-sliding-window-maximum.py
+++ src/q239-sliding-window-maximum.py
@@ -13,18 +13,14 @@ class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         q = deque()
         maximums = []
-        for i in range(k):
-            while q and q[-1][1] <= nums[i]:
-                q.pop()
-            q.append((i, nums[i]))
-        maximums.append(q[0][1])
-        for i in range(k, len(nums)):
+        for i in range(len(nums)):
             while q and q[-1][1] <= nums[i]:
                 q.pop()
             q.append((i, nums[i]))
             if q[0][0] <= i - k:
                 q.popleft()
-            maximums.append(q[0][1])
+            if i >= k - 1:
+                maximums.append(q[0][1])
         return maximums
 
 
