b2240a93dbf9f0f47e66aef4ddb1635d1b5cb69f
jiaju-yang <jiaju.yang@outlook.com>
2022-03-18 14:16:24 +0000

    Optimize the solution to question 122

--- src/q122-best-time-to-buy-and-sell-stock-ii.py
+++ src/q122-best-time-to-buy-and-sell-stock-ii.py
@@ -10,11 +10,8 @@ from typing import List
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         profit = 0
-        it = iter(enumerate(prices))
-        next(it)
-        for i, price in it:
-            if price > prices[i-1]:
-                profit += price - prices[i-1]
+        for i in range(len(prices)-1):
+            profit += max(0, prices[i+1] - prices[i])
         return profit
 
 
