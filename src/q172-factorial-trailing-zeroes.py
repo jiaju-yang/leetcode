d16e66c0c7df0f3e578706ace9a3084c8159a9fe
jiaju-yang <jiaju.yang@outlook.com>
2022-03-28 12:17:35 +0100

    Add a recursive solution to question 172

---
+++ src/q172-factorial-trailing-zeroes.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode id=172 lang=python3
+#
+# [172] Factorial Trailing Zeroes
+#
+
+# @lc code=start
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        return 0 if n == 0 else n // 5 + self.trailingZeroes(n // 5)
+
+
+# @lc code=end
+solve = Solution().trailingZeroes
+
+
+def test_default():
+    assert solve(5) == 1
+    assert solve(10) == 2
+    assert solve(11) == 2
+    assert solve(25) == 6
+
+
+def test_corner_cases():
+    assert solve(0) == 0
+    assert solve(1) == 0
+    assert solve(4) == 0
