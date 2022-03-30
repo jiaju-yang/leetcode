a38f9c9eac500c8f3c220054669771e0902fe30a
jiaju-yang <jiaju.yang@outlook.com>
2022-03-30 14:50:34 +0100

    Optimize the solution to question 179

--- src/q179-largest-number.py
+++ src/q179-largest-number.py
@@ -4,41 +4,15 @@
 # [179] Largest Number
 #
 from typing import List
+from functools import cmp_to_key
 
 
 # @lc code=start
 class Solution:
     def largestNumber(self, nums: List[int]) -> str:
-        integers = sorted(Num(num) for num in nums)
-        i = len(integers) - 1
-        while i > 0:
-            if integers[i].integer == '0':
-                integers.pop()
-            else:
-                break
-            i -= 1
-        return ''.join(str(num) for num in integers[::-1])
-
-
-class Num:
-    def __init__(self, integer) -> None:
-        self.integer = str(integer)
-
-    def __lt__(self, other):
-        i = 0
-        length = min(len(self.integer), len(other.integer))
-        while i < length:
-            if int(self.integer[i]) != int(other.integer[i]):
-                return int(self.integer[i]) < int(other.integer[i])
-            i += 1
-        if len(self.integer) < len(other.integer):
-            return Num(other.integer[i:]) > self
-        elif len(self.integer) == len(other.integer):
-            return True
-        return Num(self.integer[i:]) < other
-
-    def __str__(self) -> str:
-        return self.integer
+        if not any(nums):
+            return '0'
+        return ''.join(sorted(map(str, nums), key=cmp_to_key(lambda a, b: int(b + a) - int(a + b))))
 
 
 # @lc code=end
