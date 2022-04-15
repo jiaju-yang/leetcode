7ece368382dcb59bed5b7675d02a88e0554dad64
Jiaju <jiaju.yang@outlook.com>
2022-04-15 16:37:50 +0100

    Optimize the DP solution to question 1143

--- src/q1143-longest-common-subsequence.py
+++ src/q1143-longest-common-subsequence.py
@@ -7,15 +7,15 @@
 # @lc code=start
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        dp = [[0 for _ in range(len(text2) + 1)]
-              for _ in range(len(text1) + 1)]
-        for i in range(1, len(text1) + 1):
-            for j in range(1, len(text2) + 1):
-                if text1[i-1] == text2[j-1]:
+        m, n = len(text1), len(text2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i, c1 in enumerate(text1, start=1):
+            for j, c2 in enumerate(text2, start=1):
+                if c1 == c2:
                     dp[i][j] = dp[i-1][j-1] + 1
                 else:
                     dp[i][j] = max(dp[i-1][j], dp[i][j-1])
-        return dp[len(text1)][len(text2)]
+        return dp[-1][-1]
 
 
 # @lc code=end
