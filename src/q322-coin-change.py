2e0a22d4447a87f096e370578cc940dd28dbcd04
Jiaju <jiaju.yang@outlook.com>
2022-04-16 19:06:29 +0100

    Refactor the DP solution to question 322

--- src/q322-coin-change.py
+++ src/q322-coin-change.py
@@ -10,14 +10,13 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [-1] * (amount + 1)
+        dp = [float("inf")] * (amount + 1)
         dp[0] = 0
-        for i in range(amount):
-            if dp[i] >= 0:
-                for denomination in coins:
-                    if i + denomination <= amount and (dp[i+denomination] == -1 or dp[i+denomination] > dp[i] + 1):
-                        dp[i+denomination] = dp[i] + 1
-        return dp[amount]
+        for i in range(1, amount + 1):
+            for coin in coins:
+                if i - coin >= 0 and dp[i-coin] < dp[i] - 1:
+                    dp[i] = dp[i-coin] + 1
+        return dp[amount] if dp[amount] != float("inf") else -1
 # @lc code=end
 
 
