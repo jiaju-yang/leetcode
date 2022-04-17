5527dd77f5646182e8353189b87da20b5debdedc
Jiaju <jiaju.yang@outlook.com>
2022-04-17 14:33:09 +0100

    Refactor the DP solution to question 139

--- src/q139-word-break.py
+++ src/q139-word-break.py
@@ -11,18 +11,13 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         dp = [False] * (len(s) + 1)
         dp[0] = True
-        max_word_len = len(max(wordDict, key=len))
-        last_true = 0
-        for i in range(1, len(s) + 1):
-            if i > last_true + max_word_len:
-                break
-            for word in wordDict:
-                if i - len(word) >= 0 and dp[i-len(word)]:
-                    sub = s[i-len(word):i]
-                    if sub == word:
-                        dp[i] = True
-                        last_true = i
-                        break
+        wordDict = set(wordDict)
+        max_word_length = max(len(word) for word in wordDict)
+        for i in range(len(s)):
+            for j in range(max(0, i - max_word_length+1), i+1):
+                if dp[j] and s[j:i+1] in wordDict:
+                    dp[i+1] = True
+                    break
         return dp[-1]
 # @lc code=end
 
