a6adf4e81ed7ae4a170eb2db0b639288b0a765b9
Jiaju <jiaju.yang@outlook.com>
2022-04-17 15:27:17 +0100

    Refactor the DP solution to question 62

--- src/q62-unique-paths.py
+++ src/q62-unique-paths.py
@@ -10,14 +10,11 @@ import math
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0] * n for _ in range(m)]
-        dp[0][0] = 1
-        for row in range(m):
-            for column in range(n):
-                if column > 0:
-                    dp[row][column] += dp[row][column - 1]
-                if row > 0:
-                    dp[row][column] += dp[row-1][column]
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        dp[0][1] = 1
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[-1][-1]
 
 
