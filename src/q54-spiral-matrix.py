c8ccb3ae6d3618dca8d3c8fb82f019ca62cef23c
Jiaju <jiaju.yang@outlook.com>
2022-04-21 16:43:17 +0100

    Refactor the solution to question 54

--- src/q54-spiral-matrix.py
+++ src/q54-spiral-matrix.py
@@ -10,31 +10,8 @@ from typing import List
 
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        row_start_at, row_end_at = 0, len(matrix) - 1
-        col_start_at, col_end_at = 0, len(matrix[0]) - 1
-        result = []
-        while row_start_at < row_end_at and col_start_at < col_end_at:
-            for col in range(col_start_at, col_end_at+1):
-                result.append(matrix[row_start_at][col])
-            for row in range(row_start_at + 1, row_end_at + 1):
-                result.append(matrix[row][col_end_at])
-            for col in range(col_end_at - 1, col_start_at - 1, -1):
-                result.append(matrix[row_end_at][col])
-            for row in range(row_end_at - 1, row_start_at, -1):
-                result.append(matrix[row][col_start_at])
-            row_start_at += 1
-            row_end_at -= 1
-            col_start_at += 1
-            col_end_at -= 1
-        if row_start_at == row_end_at and col_start_at < col_end_at:
-            for col in range(col_start_at, col_end_at + 1):
-                result.append(matrix[row_start_at][col])
-        elif row_start_at < row_end_at and col_start_at == col_end_at:
-            for row in range(row_start_at, row_end_at + 1):
-                result.append(matrix[row][col_start_at])
-        elif row_start_at == row_end_at and col_start_at == col_end_at:
-            result.append(matrix[row_start_at][col_start_at])
-        return result
+        return [] if not matrix else list(matrix[0]) + self.spiralOrder(list(zip(*matrix[1:]))[::-1])
+
 # @lc code=end
 
 
