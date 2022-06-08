a637bca011e5e45588cfad8270ef6a0dee0bb125
Jiaju <jiaju.yang@outlook.com>
2022-06-08 14:10:12 +0100

    Refactor the recursive solution to question 10

--- src/q10-regular-expression-matching.py
+++ src/q10-regular-expression-matching.py
@@ -7,67 +7,19 @@
 # @lc code=start
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        pattern = self.normalize_pattern(p)
-        n, m = len(s), len(pattern)
-        dp = [[False] * (n+1) for _ in range(m+1)]
-        dp[0][0] = True
-        for i, sub_p in enumerate(pattern, start=1):
-            if len(sub_p) == 2:
-                dp[i][0] = dp[i-1][0]
-            for j, c in enumerate(s, start=1):
-                if len(sub_p) == 2:
-                    if sub_p[0] == '.':
-                        dp[i][j] = dp[i-1][j-1] or dp[i][j-1] or dp[i-1][j]
-                    else:
-                        if c == sub_p[0]:
-                            dp[i][j] = dp[i-1][j-1] or dp[i-1][j] or dp[i][j-1]
-                        else:
-                            dp[i][j] = dp[i-1][j]
-                elif sub_p == '.':
-                    dp[i][j] = dp[i-1][j-1]
-                else:
-                    if sub_p == c:
-                        dp[i][j] = dp[i-1][j-1]
-        return dp[-1][-1]
-
-    def normalize_pattern(self, p):
-        result = []
-        for c in p:
-            if c == '*':
-                result[-1] = result[-1] + c
-            else:
-                result.append(c)
-        return result
-
-    def isMatchRecursive(self, s: str, p: str) -> bool:
-        return self.drive(s, self.normalize_pattern(p))
-
-    def drive(self, s, p):
-        if not p and not s:
-            return True
-        if not s:
-            if len(p[-1]) == 2:
-                return self.drive(s, p[:-1])
-            return False
         if not p:
-            return False
-        cur_p = p[-1]
-        if len(cur_p) == 2:
-            if cur_p[0] == '.' or cur_p[0] == s[-1]:
-                return self.drive(s[:-1], p[:-1]) or self.drive(s[:-1], p) or self.drive(s, p[:-1])
-            else:
-                return self.drive(s[:], p[:-1])
+            return not s  # Brilliant base case check
+        first_match = bool(s) and p[0] in (s[0], '.')
+        if len(p) >= 2 and p[1] == '*':
+            return self.isMatch(s, p[2:]) or (first_match and self.isMatch(s[1:], p))
         else:
-            if cur_p == '.' or cur_p == s[-1]:
-                return self.drive(s[:-1], p[:-1])
-            else:
-                return False
+            return first_match and self.isMatch(s[1:], p[1:])
 
 
 # @lc code=end
 
 
-solve = Solution().isMatchRecursive
+solve = Solution().isMatch
 
 
 def test_default():
