559d63f23d271bc07ebe94fe58ecc61ea9b080a7
Jiaju <jiaju.yang@outlook.com>
2022-06-10 16:19:44 +0100

    Refactor the solution to question 45

--- src/q45-jump-game-ii.py
+++ src/q45-jump-game-ii.py
@@ -11,13 +11,12 @@ from typing import List
 # @lc code=start
 class Solution:
     def jump(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return 0
-        jumps, cur_farest, next_farest = 1, nums[0], 0
-        for i in range(1, len(nums)):
-            if i > cur_farest:
-                jumps, cur_farest = jumps + 1, next_farest
-            next_farest = max(next_farest, i + nums[i])
+        jumps, cur_jump, next_jump = 0, 0, 0
+        for i in range(len(nums) - 1):
+            next_jump = max(next_jump, i + nums[i])
+            if i == cur_jump:
+                jumps += 1
+                cur_jump = next_jump
         return jumps
 
 
