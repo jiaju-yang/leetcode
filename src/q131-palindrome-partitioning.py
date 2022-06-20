40d4e190a6bf22efd28a188760b1fcbe33a2c95c
Jiaju <jiaju.yang@outlook.com>
2022-06-20 15:34:30 +0100

    Add a DP solution to question 131

--- src/q131-palindrome-partitioning.py
+++ src/q131-palindrome-partitioning.py
@@ -4,56 +4,35 @@
 # [131] Palindrome Partitioning
 #
 from typing import List
+from collections import defaultdict
 
 # @lc code=start
 
 
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        n = len(s)
-        dp = [[False] * n for _ in range(n)]
-        for i in range(n):
-            self.extend(i, i, s, dp)
-            self.extend(i, i+1, s, dp)
-        prev_partitions = [[(0, 0)]]
-        for i in range(1, n):
-            cur_partitions = []
-            for p in prev_partitions:
-                n1 = p + [(i, i)]
-                if dp[n1[-2][0]][n1[-2][1]]:
-                    cur_partitions.append(n1)
-                n2 = p[:-1] + [(p[-1][0], i)]
-                if len(n2) == 1 or dp[n2[-2][0]][n2[-2][1]]:
-                    cur_partitions.append(n2)
-            prev_partitions = cur_partitions
-        ret = []
-        for p in prev_partitions:
-            if dp[p[-1][0]][p[-1][1]]:
-                ret.append([s[i:j+1] for i, j in p if dp[i][j]])
-        return ret
+        dp = defaultdict(list)
+        dp[0].append([])
+        for i in range(len(s)):
+            for j in range(i + 1):
+                sub = s[j:i+1]
+                if sub == sub[::-1]:
+                    dp[i+1].extend(perm + [sub] for perm in dp[j])
+        return dp[len(s)]
 
-    def extend(self, i, j, s, dp):
-        while i >= 0 and j < len(s):
-            if s[i] == s[j]:
-                dp[i][j] = True
-                i -= 1
-                j += 1
-            else:
-                break
 
 # @lc code=end
-
 
 solve = Solution().partition
 
 
 def test_default():
-    assert solve('aab') == [['a', 'a', 'b'], ['aa', 'b']]
-    assert solve('aaba') == [['a', 'a', 'b', 'a'],
-                             ['a', 'aba'], ['aa', 'b', 'a']]
+    assert solve('aab') == [['aa', 'b'], ['a', 'a', 'b']]
+    assert solve('aaba') == [['a', 'aba'], [
+        'aa', 'b', 'a'], ['a', 'a', 'b', 'a']]
 
 
 def test_corner_cases():
     assert solve('a') == [['a']]
     assert solve('ab') == [['a', 'b']]
-    assert solve('aa') == [['a', 'a'], ['aa']]
+    assert solve('aa') == [['aa'], ['a', 'a']]
